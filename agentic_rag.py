8431a311758e6aee435a48855838a80ac444006d
vamsee9201 <vamseekrishna9201@gmail.com>
2025-02-22 19:30:31 -0600

    added agent code and also agent state

--- agentic_rag.py
+++ agentic_rag.py
@@ -1,13 +1,84 @@
-from langchain_community.llms import OpenAI
+#%%
+import json
+import os
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+#%%
+with open('openai_key.json', 'r') as file:
+    data = json.load(file)
+os.environ['OPENAI_API_KEY'] = data["api_key"]
+#%%
+urls = [
+    "https://lilianweng.github.io/posts/2023-06-23-agent/",
+    "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+    "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+]
+docs = [WebBaseLoader(url).load() for url in urls]
+docs_list = [item for sublist in docs for item in sublist]
 
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=100, chunk_overlap=50
+)
+doc_splits = text_splitter.split_documents(docs_list)
+
+# Add to vectorDB
+vectorstore = Chroma.from_documents(
+    documents=doc_splits,
+    collection_name="rag-chroma",
+    embedding=OpenAIEmbeddings(),
+)
+retriever = vectorstore.as_retriever()
+#%%
+from langchain.tools.retriever import create_retriever_tool
+
+retriever_tool = create_retriever_tool(
+    retriever,
+    "retrieve_blog_posts",
+    "Search and return information about Lilian Weng blog posts on LLM agents, prompt engineering, and adversarial attacks on LLMs.",
+)
+
+tools = [retriever_tool]
+#%%
+from typing import Annotated, Sequence
+from typing_extensions import TypedDict
+
+from langchain_core.messages import BaseMessage
+
+from langgraph.graph.message import add_messages
+
+
+class AgentState(TypedDict):
+    # The add_messages function defines how an update should be processed
+    # Default is to replace. add_messages says "append"
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+#%%
+from typing import Annotated, Literal, Sequence
+from typing_extensions import TypedDict
+
+from langchain import hub
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+from pydantic import BaseModel, Field
+
+
+from langgraph.prebuilt import tools_condition
+#%%
 def generate():
 
     return ""
 
-def agent():
+def agent(state):
+    messages = state["messages"]
+    model = ChatOpenAI(temperature=0, streaming=True, model="gpt-4-turbo")
+    model = model.bind_tools(tools)
+    response = model.invoke(messages)
+    return {"messages": [response]}
 
-
-    return ""
 
 def retriever_tool():
 
