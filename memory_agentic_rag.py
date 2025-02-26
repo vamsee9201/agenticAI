8337808490efa2f0d25314a78261b2f3f813baf6
vamsee9201 <vamseekrishna9201@gmail.com>
2025-02-26 15:47:08 -0600

    found a way to customize and update state

--- memory_agentic_rag.py
+++ memory_agentic_rag.py
@@ -3,6 +3,10 @@ from langgraph.graph import MessagesState, StateGraph, START, END
 from langgraph.prebuilt import ToolNode
 from langchain_core.tools import tool
 from typing import Literal
+from langgraph.checkpoint.memory import MemorySaver
+import json
+from langchain_openai import ChatOpenAI
+from typing_extensions import TypedDict
 
 memory = MemorySaver()
 #%%
@@ -12,24 +16,53 @@ os.environ['OPENAI_API_KEY'] = data["api_key"]
 
 model = ChatOpenAI(temperature=0, streaming=True, model="gpt-4o-mini")
 #%%
-def call_model(state:MessagesState):
+from typing import Annotated, Sequence
+from typing_extensions import TypedDict
+
+from langchain_core.messages import BaseMessage
+
+from langgraph.graph.message import add_messages
+
+
+class AgentState(TypedDict):
+    # The add_messages function defines how an update should be processed
+    # Default is to replace. add_messages says "append"
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    user_question:str
+
+#%%
+def call_model(state):
     print("full messages",state["messages"])
+    try:
+        print("user question",state["user_question"])
+    except:
+        pass
     response = model.invoke(state["messages"])
     return {"messages":response}
+
+def update_user_question(state):
+    return {"user_question":"test question"}
+    
+
+
 #%%
 
-workflow = StateGraph(MessagesState)
+workflow = StateGraph(AgentState)
 
 workflow.add_node("call_model",call_model)
+workflow.add_node("update_user_question",update_user_question)
+
 workflow.add_edge(START, "call_model")
-workflow.add_edge("call_model",END)
+workflow.add_edge("call_model","update_user_question")
+workflow.add_edge("update_user_question",END)
 app = workflow.compile(checkpointer=memory)
 #%%
+from IPython.display import Image, display
 display(Image(app.get_graph(xray=True).draw_mermaid_png()))
 # %%
 from langchain_core.messages import HumanMessage
 
-config = {"configurable": {"thread_id": "3"}}
+config = {"configurable": {"thread_id": "5"}}
 input_message = HumanMessage(content="hi! I'm bob")
 for event in app.stream({"messages": [input_message]}, config, stream_mode="values"):
     event["messages"][-1].pretty_print()
