1e740d6480342804831de32df31806b312333e56
vamsee9201 <vamseekrishna9201@gmail.com>
2025-02-28 14:37:47 -0600

    need to fix out of context answers in memory agentic rag

--- memory_agentic_rag_v2.py
+++ memory_agentic_rag_v2.py
@@ -1,186 +1,193 @@
 #%%
-from langgraph.graph import MessagesState, StateGraph, START, END
-from langgraph.prebuilt import ToolNode
-from langchain_core.tools import tool
-from typing import Literal
-from langgraph.checkpoint.memory import MemorySaver
+import getpass
+import os
 import json
-from langchain_openai import ChatOpenAI
-from typing_extensions import TypedDict
-from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import WebBaseLoader
-#%%
+
 with open('openai_key.json', 'r') as file:
     data = json.load(file)
 os.environ['OPENAI_API_KEY'] = data["api_key"]
 
 #%%
-# This is vector store for the rag agentic system. 
-urls = [
-    "https://lilianweng.github.io/posts/2023-06-23-agent/",
-    "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
-    "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
-]
-docs = [WebBaseLoader(url).load() for url in urls]
-docs_list = [item for sublist in docs for item in sublist]
 
-text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-    chunk_size=100, chunk_overlap=50
-)
-doc_splits = text_splitter.split_documents(docs_list)
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
 
-# Add to vectorDB
-vectorstore = Chroma.from_documents(
-    documents=doc_splits,
-    collection_name="rag-chroma",
-    embedding=OpenAIEmbeddings(),
-)
-retriever = vectorstore.as_retriever()
-#%%
-memory = MemorySaver()
-#%%
-
-
-model = ChatOpenAI(temperature=0, streaming=True, model="gpt-4o-mini")
-#%%
-from typing import Annotated, Sequence
-from typing_extensions import TypedDict
-
-from langchain_core.messages import BaseMessage
-
-from langgraph.graph.message import add_messages
-
-
-class AgentState(TypedDict):
-    # The add_messages function defines how an update should be processed
-    # Default is to replace. add_messages says "append"
-    messages: Annotated[Sequence[BaseMessage], add_messages]
-    user_question:str
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+llm = ChatOpenAI(temperature=0, streaming=True, model="gpt-4o-mini")
 
 #%%
+from langchain_core.vectorstores import InMemoryVectorStore
 
-#tools , tools with model 
-from langchain.tools.retriever import create_retriever_tool
-from langchain_core.output_parsers import StrOutputParser
+vector_store = InMemoryVectorStore(embeddings)
+
+#%% 
+
+import bs4
 from langchain import hub
-from langchain_core.messages import AIMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from typing_extensions import List, TypedDict
 
-"""
-retriever_tool = create_retriever_tool(
-    retriever,
-    "retrieve_blog_posts",
-    "Search and return information about Lilian Weng blog posts on LLM agents, prompt engineering, and adversarial attacks on LLMs.",
+# Load and chunk contents of the blog
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
 )
-"""
+docs = loader.load()
 
-def retrieve_node(state):
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+all_splits = text_splitter.split_documents(docs)
+
+#%%
+_ = vector_store.add_documents(documents=all_splits)
+
+#%
+from langgraph.graph import MessagesState, StateGraph
+
+graph_builder = StateGraph(MessagesState)
+#%%
+from langchain_core.tools import tool
+
+@tool(response_format="content_and_artifact")
+def retrieve(query: str):
     """Retrieve information related to a query."""
-    retrieved_docs = vectorstore.similarity_search(state["user_question"], k=2)
+    retrieved_docs = vector_store.similarity_search(query, k=2)
     serialized = "\n\n".join(
         (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
         for doc in retrieved_docs
     )
-    return {"messages":[serialized]}
-
-def generate(state):
-    print("---GENERATE---")
-    messages = state["messages"]
-    print("This is generate node")
-    print("full messages :",messages)
-    question = state["user_question"]
-    print("question:",question)
-    last_message = messages[-1]
-    docs = last_message.content
-    prompt = hub.pull("rlm/rag-prompt")
-    llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0, streaming=True)
-    def format_docs(docs):
-        return "\n\n".join(doc.page_content for doc in docs)
-    rag_chain = prompt | llm | StrOutputParser()
-    response = rag_chain.invoke({"context": docs, "question": question})
-    print("response:",response)
-    return {"messages": [AIMessage(content=response)]}
-
-
-
-
-
+    return serialized, retrieved_docs
 #%%
+from langchain_core.messages import SystemMessage
+from langgraph.prebuilt import ToolNode
 
 
+# Step 1: Generate an AIMessage that may include a tool-call to be sent.
+def query_or_respond(state: MessagesState):
+    """Generate tool call for retrieval or respond."""
+    llm_with_tools = llm.bind_tools([retrieve])
+    response = llm_with_tools.invoke(state["messages"])
+    # MessagesState appends messages to state instead of overwriting
+    return {"messages": [response]}
 
-def update_user_question(state):
-    #print(str(state["messages"]))
-    conversation_history = "\n".join(
-        f"{'Human:' if isinstance(message, HumanMessage) else 'AI:'} {message.content}" 
-        for message in state["messages"]
+
+# Step 2: Execute the retrieval.
+tools = ToolNode([retrieve])
+
+
+# Step 3: Generate a response using the retrieved content.
+def generate(state: MessagesState):
+    """Generate answer."""
+    # Get generated ToolMessages
+    recent_tool_messages = []
+    for message in reversed(state["messages"]):
+        if message.type == "tool":
+            recent_tool_messages.append(message)
+        else:
+            break
+    tool_messages = recent_tool_messages[::-1]
+
+    # Format into prompt
+    docs_content = "\n\n".join(doc.content for doc in tool_messages)
+    system_message_content = (
+        "You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise."
+        "\n\n"
+        "context:"
+        "\n\n"
+        f"{docs_content}"
     )
-    print("conversation history :",conversation_history + "end of conversation history")
-    prompt = f"""
-    Rewrite the latest human question to be fully self-contained while preserving its meaning. 
-    If it refers to context from the conversation, replace pronouns or vague terms with the correct reference. 
-    If it is already clear, keep it unchanged. Do not add explanations or modify intent.      
-    Conversation history: 
-    {conversation_history}
-    """
-    response = model.invoke(prompt)
-    return {"user_question":response.content}
+    conversation_messages = [
+        message
+        for message in state["messages"]
+        if message.type in ("human", "system")
+        or (message.type == "ai" and not message.tool_calls)
+    ]
+    prompt = [SystemMessage(system_message_content)] + conversation_messages
 
-#tools = [update_user_question]
-
-#model_tools = model.bind_functions(tools)
-def call_model(state):
-    print("full messages",state["messages"])
-    try:
-        print("user question",state["user_question"])
-    except:
-        pass
-    response = model.invoke(state["messages"])
-    return {"messages":response}
-    
-
-
+    # Run
+    response = llm.invoke(prompt)
+    return {"messages": [response]}
 #%%
-from langgraph.prebuilt import tools_condition
-workflow = StateGraph(AgentState)
+from langgraph.graph import END
+from langgraph.prebuilt import ToolNode, tools_condition
 
-#workflow.add_node("call_model",call_model)
-workflow.add_node("update_user_question",update_user_question)
-workflow.add_node("retrieve_node",retrieve_node)
-workflow.add_node("generate",generate)
+graph_builder.add_node(query_or_respond)
+graph_builder.add_node(tools)
+graph_builder.add_node(generate)
 
-#  workflow.add_edge(START, "call_model")
-workflow.add_edge(START,"update_user_question")
-workflow.add_edge("update_user_question","retrieve_node")
-workflow.add_edge("retrieve_node","generate")
-workflow.add_edge("generate",END)
-app = workflow.compile(checkpointer=memory)
+graph_builder.set_entry_point("query_or_respond")
+graph_builder.add_conditional_edges(
+    "query_or_respond",
+    tools_condition,
+    {END: END, "tools": "tools"},
+)
+graph_builder.add_edge("tools", "generate")
+graph_builder.add_edge("generate", END)
+
+graph = graph_builder.compile()
 #%%
 from IPython.display import Image, display
-display(Image(app.get_graph(xray=True).draw_mermaid_png()))
-# %%
-from langchain_core.messages import HumanMessage
 
-config = {"configurable": {"thread_id": "3"}}
-input_message = HumanMessage(content="who is lilian weng")
-for event in app.stream({"messages": [input_message]}, config, stream_mode="values"):
-    event["messages"][-1].pretty_print()
+display(Image(graph.get_graph().draw_mermaid_png()))
+
+
+
+
+# %%
+input_message = "Hello"
+
+for step in graph.stream(
+    {"messages": [{"role": "user", "content": input_message}]},
+    stream_mode="values",
+):
+    step["messages"][-1].pretty_print()
+# %%
+input_message = "What is Task Decomposition?"
+
+for step in graph.stream(
+    {"messages": [{"role": "user", "content": input_message}]},
+    stream_mode="values",
+):
+    step["messages"][-1].pretty_print()
+# %%
+from langgraph.checkpoint.memory import MemorySaver
+
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
+
+# Specify an ID for the thread
+config = {"configurable": {"thread_id": "abc123"}}  
 #%%
+input_message = "What is Task Decomposition?"
 
-input_message = HumanMessage(content="what did she say about agent memory?")
-for event in app.stream({"messages": [input_message]}, config, stream_mode="values"):
-    event["messages"][-1].pretty_print()
-app.get_state(config=config)
+for step in graph.stream(
+    {"messages": [{"role": "user", "content": input_message}]},
+    stream_mode="values",
+    config=config,
+):
+    step["messages"][-1].pretty_print()
+#%%
+input_message = "Can you look up some common ways of doing it?"
 
+for step in graph.stream(
+    {"messages": [{"role": "user", "content": input_message}]},
+    stream_mode="values",
+    config=config,
+):
+    step["messages"][-1].pretty_print()
 # %%
 
-input_message = HumanMessage(content="what did she say about adversarial attacks?")
-for event in app.stream({"messages": [input_message]}, config, stream_mode="values"):
-    event["messages"][-1].pretty_print()
-app.get_state(config=config)
+input_message = "Who is elon musk?"
 
-#testing commits
-#Now i have to add a tool to the model that can retrieve from a vector database. 
-
+for step in graph.stream(
+    {"messages": [{"role": "user", "content": input_message}]},
+    stream_mode="values",
+    config=config,
+):
+    step["messages"][-1].pretty_print()
 # %%
