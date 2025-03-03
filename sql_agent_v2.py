f5ea4093cb857bfd02ac24be3df46f3d626374af
vamsee9201 <vamseekrishna9201@gmail.com>
2025-03-02 22:38:25 -0600

    added the ability to query a sq lite database.

---
+++ sql_agent_v2.py
@@ -0,0 +1,7 @@
+#%%
+from langchain_community.utilities import SQLDatabase
+
+db = SQLDatabase.from_uri("sqlite:///Chinook.db")
+print(db.dialect)
+print(db.get_usable_table_names())
+db.run("SELECT * FROM Artist LIMIT 10;")
