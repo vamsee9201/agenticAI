881c08b64afd03a70883ea64630a999cd84f0a88
vamsee9201 <vamseekrishna9201@gmail.com>
2025-03-02 22:47:29 -0600

    added ability to generate query

--- sql_agent_v2.py
+++ sql_agent_v2.py
@@ -5,3 +5,64 @@ db = SQLDatabase.from_uri("sqlite:///Chinook.db")
 print(db.dialect)
 print(db.get_usable_table_names())
 db.run("SELECT * FROM Artist LIMIT 10;")
+# %%
+from typing_extensions import TypedDict
+class State(TypedDict):
+    question: str
+    query: str
+    result: str
+    answer: str
+#%%
+import getpass
+import os
+import json
+#%%
+with open('openai_key.json', 'r') as file:
+    data = json.load(file)
+os.environ['OPENAI_API_KEY'] = data["api_key"]
+#%%
+from langchain.chat_models import init_chat_model
+llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+#%%
+from langchain import hub
+
+query_prompt_template = hub.pull("langchain-ai/sql-query-system-prompt")
+
+assert len(query_prompt_template.messages) == 1
+query_prompt_template.messages[0].pretty_print()
+#%%
+from typing_extensions import Annotated
+
+
+class QueryOutput(TypedDict):
+    """Generated SQL query."""
+
+    query: Annotated[str, ..., "Syntactically valid SQL query."]
+#%%
+print(db.get_table_info())
+print(db.dialect)
+
+#%%
+def write_query(state: State):
+    """Generate SQL query to fetch information."""
+    prompt = query_prompt_template.invoke(
+        {
+            "dialect": db.dialect,
+            "top_k": 10,
+            "table_info": db.get_table_info(),
+            "input": state["question"],
+        }
+    )
+    structured_llm = llm.with_structured_output(QueryOutput)
+    result = structured_llm.invoke(prompt)
+    return {"query": result["query"]}
+#%%
+write_query({"question": "How many Employees are there?"})
+# %%
+from langchain_community.tools.sql_database.tool import QuerySQLDatabaseTool
+
+
+def execute_query(state: State):
+    """Execute SQL query."""
+    execute_query_tool = QuerySQLDatabaseTool(db=db)
+    return {"result": execute_query_tool.invoke(state["query"])}
